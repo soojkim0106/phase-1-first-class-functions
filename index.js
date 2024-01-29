89f2cdf66e7d0b246ed2cfbfd5d4f664073d0efc
Soojungkim <soojung.kimm@gmail.com>
2024-01-29 15:33:57 -0800

    complete lab

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){;
+    callback();
+}
+
+function returnsANamedFunction(){
+    return function namedFunction(){
+
+    }
+}
+
+function returnsAnAnonymousFunction(){
+
+    return function(){
+
+    }
+}
